8e406d5b98e40d740ce819aa4c66feaf3f70038f
Arkaion <thibault.arnould@wanadoo.fr>
2018-09-05 17:17:07 +0200

    Fin du cours

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thiba
- * Date: 05/09/2018
- * Time: 14:49
- */
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class LuckyController
+{
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
---
+++ src/Toto/Toto.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thiba
+ * Date: 05/09/2018
+ * Time: 17:03
+ */
+
+namespace App\Toto;
+
+
+class Toto
+{
+
+}
